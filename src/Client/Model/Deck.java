9e6a705fe663a6e420ad9f09abe7ce122f7a5bc1
mehreganmohseni <momohammadi@ce.sharif.edu>
2019-12-18 14:05:36 +0330

    deck, hand

--- src/Client/Model/Deck.java
+++ src/Client/Model/Deck.java
@@ -1,4 +1,15 @@
 package Client.Model;
 
+import java.util.List;
+
 public class Deck {
+    private List<Deck> decks;
+
+    public List<Deck> getDecks() {
+        return decks;
+    }
+
+    public void setDecks(List<Deck> decks) {
+        this.decks = decks;
+    }
 }
