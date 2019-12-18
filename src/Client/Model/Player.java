6e8525d40309d0c6976193d62b2728dba731a59c
mehreganmohseni <momohammadi@ce.sharif.edu>
2019-12-18 13:58:17 +0330

    map, unit, player

--- src/Client/Model/Player.java
+++ src/Client/Model/Player.java
@@ -1,4 +1,60 @@
 package Client.Model;
 
+import java.util.List;
+
 public class Player {
+    private int playerID, AP;
+    private boolean upgradeTokens;
+    private Deck deck;
+    private Hand hand;
+    private List<Spell> spells;
+
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public int getAP() {
+        return AP;
+    }
+
+    public void setAP(int AP) {
+        this.AP = AP;
+    }
+
+    public boolean isUpgradeTokens() {
+        return upgradeTokens;
+    }
+
+    public void setUpgradeTokens(boolean upgradeTokens) {
+        this.upgradeTokens = upgradeTokens;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public List<Spell> getSpells() {
+        return spells;
+    }
+
+    public void setSpells(List<Spell> spells) {
+        this.spells = spells;
+    }
 }
--- src/Client/Model/Unit.java
+++ src/Client/Model/Unit.java
@@ -1,4 +1,65 @@
 package Client.Model;
 
 public class Unit {
+    private int unitID, hp, playerID;
+    private boolean isHasted;
+    private Cell cell;
+    private BaseUnit baseUnit;
+    private Path path;
+
+    public int getUnitID() {
+        return unitID;
+    }
+
+    public void setUnitID(int unitID) {
+        this.unitID = unitID;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public boolean isHasted() {
+        return isHasted;
+    }
+
+    public void setHasted(boolean hasted) {
+        isHasted = hasted;
+    }
+
+    public Cell getCell() {
+        return cell;
+    }
+
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+
+    public BaseUnit getBaseUnit() {
+        return baseUnit;
+    }
+
+    public void setBaseUnit(BaseUnit baseUnit) {
+        this.baseUnit = baseUnit;
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
+    }
 }
