0cd8c4e8ea0fd324f1c682c8436c150dd770b372
mehreganmohseni <isavand.alireza@gmail.com>
2019-12-25 23:53:02 +0330

    getter and setters of turn message

--- src/Client/dto/ClientCell.java
+++ src/Client/dto/ClientCell.java
@@ -3,4 +3,20 @@ package Client.dto;
 public class ClientCell {
     private int row;
     private int col;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
--- src/Client/dto/turn/TurnKing.java
+++ src/Client/dto/turn/TurnKing.java
@@ -4,4 +4,28 @@ public class TurnKing {
     private int playerId;
     private boolean isAlive;
     private int hp;
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
