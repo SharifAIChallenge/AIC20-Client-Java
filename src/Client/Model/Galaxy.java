d3626f30e27128fd72625ac298ad8bedd15f755b
mehreganmohseni <isavand.alireza@gmail.com>
2019-12-26 01:41:01 +0330

    create TurnMessage

--- src/Client/Model/Galaxy.java
+++ src/Client/Model/Galaxy.java
@@ -1,9 +1,12 @@
 package Client.Model;
 
 import Client.dto.init.ClientInitMessage;
+import Client.dto.turn.ClientTurnMessage;
 
 public class Galaxy implements World {
-    private ClientInitMessage clientInitMessage
+    private ClientInitMessage clientInitMessage;
+    private ClientTurnMessage clientTurnMessage;
+
 
     public ClientInitMessage getClientInitMessage() {
         return clientInitMessage;
