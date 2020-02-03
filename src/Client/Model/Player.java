febce5915692e8233dced612d4d09e083444d84b
mehreganmohseni <isavand.alireza@gmail.com>
2020-02-03 00:57:58 -0800

    player was updated

--- src/Client/Model/Player.java
+++ src/Client/Model/Player.java
@@ -4,88 +4,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    private int playerID, AP;
-    private boolean upgradeTokens;
-    private List<Spell> spells;
-    private List<Unit> hand;
-    private List<Unit> deck;
-    private List<Unit> playerUnits = new ArrayList<>();
-    private Path[][] shortestPathsToCells;
-    private CastAreaSpell castAreaSpell;
-    private CastUnitSpell castUnitSpell;
-    private boolean isCalcCastAreaSpell = false;
-    private boolean isCalcCastUnitSpell = false;
-    private List<Unit> turnPlayedUnits = new ArrayList<>();
+    private int playerId;
+    private List<BaseUnit> deck;
+    private List<BaseUnit> hand;
+    private int ap;
+    private King king;
     private List<Path> pathsFromPlayer = new ArrayList<>();
     private Path pathToFriend;
+    private List<Unit> units = new ArrayList<>();
+    private CastAreaSpell castAreaSpell;
+    private CastUnitSpell castUnitSpell;
+    private List<Unit> duplicateUnits;
+    private List<Unit> hastedUnits;
+    private List<Unit> playedUnits = new ArrayList<>();
+    private List<Unit> diedUnits;
+    private Unit rangeUpgradedUnit;
+    private Unit damageUpgradedUnit;
 
-    public Player(int playerID){
-        this.playerID = playerID;
+    ////////
+    private Path[][] shortestPathsToCells;
+    private boolean isCalcCastAreaSpell = false;
+    private boolean isCalcCastUnitSpell = false;
+////////
+
+    public Player(int playerID) {
+        this.playerId = playerID;
     }
 
-    public int getPlayerID() {
-        return playerID;
+    public int getPlayerId() {
+        return playerId;
     }
 
-    public void setPlayerID(int playerID) {
-        this.playerID = playerID;
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
     }
 
-    public int getAP() {
-        return AP;
+    public int getAp() {
+        return ap;
     }
 
-    public void setAP(int AP) {
-        this.AP = AP;
+    public void setAp(int ap) {
+        this.ap = ap;
     }
 
-    public boolean isUpgradeTokens() {
-        return upgradeTokens;
-    }
-
-    public void setUpgradeTokens(boolean upgradeTokens) {
-        this.upgradeTokens = upgradeTokens;
-    }
-
-
-    public List<Spell> getSpells() {
-        return spells;
-    }
-
-    public void setSpells(List<Spell> spells) {
-        this.spells = spells;
-    }
-
-    public List<Unit> getHand() {
+    public List<BaseUnit> getHand() {
         return hand;
     }
 
-    public void setHand(List<Unit> hand) {
+    public void setHand(List<BaseUnit> hand) {
         this.hand = hand;
     }
 
-    public List<Unit> getDeck() {
-        return deck;
+    public List<Unit> getUnits() {
+        return units;
     }
 
-    public void setDeck(List<Unit> deck) {
-        this.deck = deck;
-    }
-
-    public List<Unit> getPlayerUnits() {
-        return playerUnits;
-    }
-
-    public void setPlayerUnits(List<Unit> playerUnits) {
-        this.playerUnits = playerUnits;
-    }
-
-    public Path[][] getShortestPathsToCells() {
-        return shortestPathsToCells;
-    }
-
-    public void setShortestPathsToCells(Path[][] shortestPathsToCells) {
-        this.shortestPathsToCells = shortestPathsToCells;
+    public void setUnits(List<Unit> units) {
+        this.units = units;
     }
 
     public CastAreaSpell getCastAreaSpell() {
@@ -104,28 +79,12 @@ public class Player {
         this.castUnitSpell = castUnitSpell;
     }
 
-    public boolean isCalcCastAreaSpell() {
-        return isCalcCastAreaSpell;
+    public List<Unit> getPlayedUnits() {
+        return playedUnits;
     }
 
-    public void setCalcCastAreaSpell(boolean calcCastAreaSpell) {
-        isCalcCastAreaSpell = calcCastAreaSpell;
-    }
-
-    public boolean isCalcCastUnitSpell() {
-        return isCalcCastUnitSpell;
-    }
-
-    public void setCalcCastUnitSpell(boolean calcCastUnitSpell) {
-        isCalcCastUnitSpell = calcCastUnitSpell;
-    }
-
-    public List<Unit> getTurnPlayedUnits() {
-        return turnPlayedUnits;
-    }
-
-    public void setTurnPlayedUnits(List<Unit> turnPlayedUnits) {
-        this.turnPlayedUnits = turnPlayedUnits;
+    public void setPlayedUnits(List<Unit> playedUnits) {
+        this.playedUnits = playedUnits;
     }
 
     public List<Path> getPathsFromPlayer() {
@@ -142,5 +101,95 @@ public class Player {
 
     public void setPathToFriend(Path pathToFriend) {
         this.pathToFriend = pathToFriend;
+    }
+
+    public List<BaseUnit> getDeck() {
+        return deck;
+    }
+
+    public void setDeck(List<BaseUnit> deck) {
+        this.deck = deck;
+    }
+
+    public King getKing() {
+        return king;
+    }
+
+    public void setKing(King king) {
+        this.king = king;
+    }
+
+    public List<Unit> getDuplicateUnits() {
+        return duplicateUnits;
+    }
+
+    public void setDuplicateUnits(List<Unit> duplicateUnits) {
+        this.duplicateUnits = duplicateUnits;
+    }
+
+    public List<Unit> getHastedUnits() {
+        return hastedUnits;
+    }
+
+    public void setHastedUnits(List<Unit> hastedUnits) {
+        this.hastedUnits = hastedUnits;
+    }
+
+    public List<Unit> getDiedUnits() {
+        return diedUnits;
+    }
+
+    public void setDiedUnits(List<Unit> diedUnits) {
+        this.diedUnits = diedUnits;
+    }
+
+    public Unit getRangeUpgradedUnit() {
+        return rangeUpgradedUnit;
+    }
+
+    public void setRangeUpgradedUnit(Unit rangeUpgradedUnit) {
+        this.rangeUpgradedUnit = rangeUpgradedUnit;
+    }
+
+    public Unit getDamageUpgradedUnit() {
+        return damageUpgradedUnit;
+    }
+
+    public void setDamageUpgradedUnit(Unit damageUpgradedUnit) {
+        this.damageUpgradedUnit = damageUpgradedUnit;
+    }
+
+    /////////////
+    boolean isCalcCastAreaSpell() {
+        return isCalcCastAreaSpell;
+    }
+
+    void setCalcCastAreaSpell(boolean calcCastAreaSpell) {
+        isCalcCastAreaSpell = calcCastAreaSpell;
+    }
+
+    boolean isCalcCastUnitSpell() {
+        return isCalcCastUnitSpell;
+    }
+
+    void setCalcCastUnitSpell(boolean calcCastUnitSpell) {
+        isCalcCastUnitSpell = calcCastUnitSpell;
+    }
+
+    Path[][] getShortestPathsToCells() {
+        return shortestPathsToCells;
+    }
+
+    void setShortestPathsToCells(Path[][] shortestPathsToCells) {
+        this.shortestPathsToCells = shortestPathsToCells;
+    }
+    ///////////////////////
+
+    public boolean isAlive() {
+        return king.isAlive();
+    }
+
+    public int getHp(){
+        return king.getHp();
     }
 }
